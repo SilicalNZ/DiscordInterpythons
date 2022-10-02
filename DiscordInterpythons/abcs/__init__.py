d481a23a3be2f79414785408238acf5eddb5430f
SilicalNZ <SilicalNZ@gmail.com>
2022-10-02 21:57:52 +0200

    Add missing ABC imports

--- DiscordInterpythons/abcs/__init__.py
+++ DiscordInterpythons/abcs/__init__.py
@@ -1,3 +1,8 @@
-from .channel import *
-from .message import *
-from .thread import *
+from .application_command import DiscordApplicationCommandABC
+from .channel import ChannelABC
+from .message import MessageABC
+from .thread import ThreadABC
+from .webhook import (
+    WebhookAuthABC,
+    WebhookABC,
+)
